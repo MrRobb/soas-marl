6bfaaec7452ed58da6484011d53cfbaac9b579e2
Mike <michael.birkholz@estudiantat.upc.edu>
2022-05-26 11:42:48 +0200

    Fix some more review items

--- src/QAgent.hpp
+++ src/QAgent.hpp
@@ -19,11 +19,9 @@
 #include "Tester.hpp"
 
 
-class QAgent {
+class QAgent : Agent {
 private:
     int agent_id;
-    int s_i; // index of initial state
-    MachineState s;
     std::vector<AgentAction_e> actions;
     unsigned int num_states;
     
@@ -37,22 +35,32 @@ private:
     
 public:
     
-    QAgent(SparseRewardMachineFlavor_e reward_machine, MachineState s_i, unsigned int num_states, std::vector<AgentAction_e> actions, int agent_id) :
+    QAgent(SparseRewardMachineFlavor_e reward_machine, unsigned int num_states, std::vector<AgentAction_e> actions, int agent_id, ButtonsEnvironment &_env) :
+        Agent(_env),
         rm (std::stringstream(SparseRewardMachine::getRmBuf(reward_machine))) {
         this->agent_id = agent_id;
-        this->s_i = s_i;
-        this->s = s_i;
         this->actions = actions;
         this->num_states = num_states;
-        
         this->u = this->rm.get_initial_state();
         this->local_event_set = this->rm.get_events();
-        
+        this->q = xt::zeros<double>({int(num_states), int(this->rm.get_states().size()), int(this->actions.size())});
+
+    }
+
+    QAgent(SparseRewardMachineFlavor_e reward_machine, unsigned int num_states, std::vector<AgentAction_e> actions, int agent_id, ButtonsEnvironment &_env, ofVec2f _init_loc) :
+        Agent(_env, _init_loc),
+        rm (std::stringstream(SparseRewardMachine::getRmBuf(reward_machine))) {
+        this->agent_id = agent_id;
+        this->actions = actions;
+        this->num_states = num_states;
+        this->u = this->rm.get_initial_state();
+        this->local_event_set = this->rm.get_events();
         this->q = xt::zeros<double>({int(num_states), int(this->rm.get_states().size()), int(this->actions.size())});
     }
+
     
     void reset_state() {
-        this->s = this->s_i;
+        // Environment should reset position. Can probably delete this
     }
     
     void initialize_reward_machine() {
@@ -69,7 +77,8 @@ public:
         }
     }
     
-    std::pair<MachineState, AgentAction_e> get_next_action(const double epsilon, LearningParameters& learning_params) {
+    AgentAction_e get_next_action(const double epsilon, LearningParameters& learning_params)
+    {
         double T = learning_params.getT();
         AgentAction_e a_selected = STAY;
         
@@ -80,8 +89,8 @@ public:
         }
         else {
             // Exploit
-            auto pr_sum = xt::sum(xt::exp(xt::view(this->q, this->s, this->u, xt::all()) * T));
-            xt::xarray<double> pr = xt::exp(xt::view(this->q, this->s, this->u, xt::all()) * T) / pr_sum;
+            auto pr_sum = xt::sum(xt::exp(xt::view(this->q, getAgentState(), this->u, xt::all()) * T));
+            xt::xarray<double> pr = xt::exp(xt::view(this->q, getAgentState(), this->u, xt::all()) * T) / pr_sum;
             
             // If any q-values are so large that the softmax function returns infinity,
             // make the corresponding actions equally likely
@@ -106,7 +115,7 @@ public:
             }
         }
         
-        return std::make_pair(this->s, a_selected);
+        return a_selected;
     }
     
     void update_agent(const MachineState& s_new, const AgentAction_e& a, const Reward& reward, const std::vector<Event>& label, LearningParameters& learning_params, bool update_q_function = true) {
@@ -120,10 +129,13 @@ public:
         this->total_local_reward += reward;
         
         if (update_q_function) {
-            this->update_q_function(this->s, s_new, u_start, this->u, a, reward, learning_params);
+            this->update_q_function(getAgentState(), s_new, u_start, this->u, a, reward, learning_params);
         }
         
-        this->s = s_new;
+        // Note: performAction does this (as well as test validity). May need to combine/change somehow.
+        //       env.get_next_state does slip plus validity check
+        //       env.environment_step does get_next_state, MDP label, rm: get_next_state and reward
+        //this->s = s_new;
         
         if (this->rm.is_terminal_state(this->u)) {
             this->is_task_complete = true;
@@ -135,6 +147,6 @@ public:
         double gamma = learning_params.getGamma();
         
         double bellman = xt::amax(xt::view(this->q, s_new, u_new, xt::all()))[0];
-        this->q(s, u, a) = (1.0 - alpha) * this->q(s, u, a) + alpha * (reward + gamma * bellman);
+        this->q(getAgentState(), u, a) = (1.0 - alpha) * this->q(getAgentState(), u, a) + alpha * (reward + gamma * bellman);
     }
 };
